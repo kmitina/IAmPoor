b3fa2e08cabc45fb6cfba60be12f218e60e76164
kris <kristmitina@gmail.com>
2020-06-12 17:01:51 +0300

    release v1

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by DVKSH on 09.06.2020.
+//  Copyright © 2020 Kris Mitina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
